fcf59c85dfae9f59c75d03f319c2fc8b52347fc2
qiufeng <qiufeng9862@qq.com>
2022-09-08 17:53:47 +0800

    完善日志记录逻辑

--- mf-code-create/src/main/java/cn/com/mfish/code/MfCodeApplication.java
+++ mf-code-create/src/main/java/cn/com/mfish/code/MfCodeApplication.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * @author ：qiufeng
@@ -18,7 +17,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @Slf4j
 @MapperScan({"cn.com.mfish.code.mapper"})
 @AutoFeignClients
-@EnableAsync
 public class MfCodeApplication {
     public static void main(String[] args) {
         SpringApplication.run(MfCodeApplication.class, args);
