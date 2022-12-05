740149bc42c50d0a80158afc2ab663d92fc596a9
qiufeng <qiufeng9862@qq.com>
2022-12-05 18:33:34 +0800

    feat: 增加统一权限切面

---
+++ mf-common/mf-common-oauth/src/main/java/cn/com/mfish/oauth/aspect/AuthorizationAspect.java
@@ -0,0 +1,36 @@
+package cn.com.mfish.oauth.aspect;
+
+import cn.com.mfish.oauth.annotation.RequiresPermissions;
+import cn.com.mfish.oauth.annotation.RequiresRoles;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author ：qiufeng
+ * @description：权限切面处理
+ * @date ：2022/12/5 18:04
+ */
+@Aspect
+@Component
+@Slf4j
+public class AuthorizationAspect {
+    @Before("@annotation(cn.com.mfish.oauth.annotation.RequiresPermissions)||@annotation(cn.com.mfish.oauth.annotation.RequiresRoles)")
+    public void doBefore(JoinPoint joinPoint) {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        Method method = methodSignature.getMethod();
+        if (method.isAnnotationPresent(RequiresPermissions.class)) {
+            RequiresPermissions requiresPermissions = method.getAnnotation(RequiresPermissions.class);
+            System.out.println(requiresPermissions.value()+"  RequiresPermissions");
+        }
+        if (method.isAnnotationPresent(RequiresRoles.class)) {
+            System.out.println("RequiresRoles");
+        }
+    }
+
+}
