9ca2b5e152805d92b8517b955500340918431a9d
qiufeng <qiufeng9862@qq.com>
2022-12-16 18:26:19 +0800

    fix: 解决打包问题

--- mf-business/mf-web/src/main/java/MfWebApplication.java
+++ mf-business/mf-web/src/main/java/MfWebApplication.java
@@ -1,21 +1,18 @@
-package cn.com.mfish.sys;
-
 import cn.com.mfish.common.web.annotation.AutoWeb;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 
 /**
  * @author ：qiufeng
- * @description：系统管理启动类
- * @date ：2022/9/2 16:03
+ * @description：其他web业务服务参考类
+ * @date ：2022/12/16 10:01
  */
-
 @Slf4j
 @AutoWeb
-public class MfSysApplication {
+public class MfWebApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MfSysApplication.class, args);
+        SpringApplication.run(MfWebApplication.class, args);
         log.info("\n\t----------------------------------------------------------\n\t" +
-                "\n\t--------------------摸鱼系统业务服务启动成功-----------------------\n\t");
+                "\n\t--------------------摸鱼web业务服务启动成功-----------------------\n\t");
     }
 }
