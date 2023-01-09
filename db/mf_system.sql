5e46e40703939bf73835704813448d9d56dced5e
qiufeng <qiufeng9862@qq.com>
2023-01-09 22:40:22 +0800

    feat: 数据库调整

--- db/mf_system.sql
+++ db/mf_system.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50734
  File Encoding         : 65001
 
- Date: 07/01/2023 23:17:39
+ Date: 09/01/2023 22:39:42
 */
 
 SET NAMES utf8mb4;
@@ -24,7 +24,7 @@ DROP TABLE IF EXISTS `sys_dict`;
 CREATE TABLE `sys_dict`  (
   `id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '唯一ID',
   `dict_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '字典名称',
-  `dict_code` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '字典编码',
+  `dict_code` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '字典编码',
   `status` int(1) NULL DEFAULT 0 COMMENT '状态(0正常 1停用)',
   `remark` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '备注',
   `create_by` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '创建用户',
@@ -32,14 +32,16 @@ CREATE TABLE `sys_dict`  (
   `update_by` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '更新用户',
   `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `dict_type`(`dict_code`) USING BTREE
+  UNIQUE INDEX `dict_code_index`(`dict_code`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '字典表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_dict
 -- ----------------------------
 INSERT INTO `sys_dict` VALUES ('193585c2417cfb3e4865501c2e608602', '数据库类型', 'sys_db_type', 0, '支持的数据库类型', 'admin', '2023-01-03 11:45:12', 'admin', '2023-01-03 11:46:12');
+INSERT INTO `sys_dict` VALUES ('5665dd400700ebea77fcc6f8e39fc355', '请求类型', 'request_type', 0, 'http请求类型', 'admin', '2023-01-09 15:53:31', '', NULL);
 INSERT INTO `sys_dict` VALUES ('ad7336dda270e6430565b313a741ffb7', '用户性别', 'sys_user_sex', 0, '用户性别字典', 'admin', '2023-01-04 16:08:35', 'admin', '2023-01-05 14:47:40');
+INSERT INTO `sys_dict` VALUES ('c694f0f6feba27026044839b77d24caa', '日志操作类型', 'sys_log_type', 0, '日志操作类型', 'admin', '2023-01-09 11:27:05', '', NULL);
 
 -- ----------------------------
 -- Table structure for sys_dict_item
@@ -47,26 +49,42 @@ INSERT INTO `sys_dict` VALUES ('ad7336dda270e6430565b313a741ffb7', '用户性别
 DROP TABLE IF EXISTS `sys_dict_item`;
 CREATE TABLE `sys_dict_item`  (
   `id` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '唯一ID',
-  `dict_code` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '字典编码',
+  `dict_code` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '字典编码',
   `dict_label` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '字典标签',
   `dict_value` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '字典键值',
   `dict_sort` int(11) NULL DEFAULT 0 COMMENT '字典排序',
+  `color` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '配色(用于前端显示)',
   `status` int(1) NULL DEFAULT 0 COMMENT '状态(0正常 1停用)',
   `remark` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '备注',
   `create_by` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '创建用户',
   `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
   `update_by` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '更新用户',
   `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`) USING BTREE
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `dict_code_index`(`dict_code`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '字典项' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_dict_item
 -- ----------------------------
-INSERT INTO `sys_dict_item` VALUES ('0f11bb06b93b05e27f8a45d78765a243', 'sys_db_type', 'mysql', 'mysql', 1, 0, 'mysql数据库', 'admin', '2023-01-03 16:40:55', 'admin', '2023-01-04 16:17:07');
-INSERT INTO `sys_dict_item` VALUES ('8d0e789e6e63e994b31750ad9cb637b4', 'sys_db_type', 'oracle', 'oracle', 2, 0, 'oracle数据库', 'admin', '2023-01-04 17:32:59', '', NULL);
-INSERT INTO `sys_dict_item` VALUES ('ce8059c1fb3b533339f6bdccc17e0046', 'sys_user_sex', '男', '1', 1, 0, '男性', 'admin', '2023-01-04 17:31:35', 'admin', '2023-01-04 17:32:00');
-INSERT INTO `sys_dict_item` VALUES ('ec9cfc538a91de2f61829e61064d636b', 'sys_user_sex', '女', '1', 2, 0, '女性', 'admin', '2023-01-04 17:31:53', '', NULL);
+INSERT INTO `sys_dict_item` VALUES ('0625b88c24a5cd98e045a58679370249', 'sys_log_type', '新增', '新增', 3, 'blue', 0, NULL, 'admin', '2023-01-09 11:27:52', 'admin', '2023-01-09 16:11:41');
+INSERT INTO `sys_dict_item` VALUES ('0f11bb06b93b05e27f8a45d78765a243', 'sys_db_type', 'mysql', 'mysql', 1, NULL, 0, 'mysql数据库', 'admin', '2023-01-03 16:40:55', 'admin', '2023-01-04 16:17:07');
+INSERT INTO `sys_dict_item` VALUES ('2065051b945805f8915ea68431c097ff', 'sys_log_type', '查询', '查询', 2, 'green', 0, NULL, 'admin', '2023-01-09 11:27:46', 'admin', '2023-01-09 15:21:07');
+INSERT INTO `sys_dict_item` VALUES ('261b1fdc50de63e495cdb757b6d430e2', 'sys_log_type', '删除', '删除', 5, 'red', 0, NULL, 'admin', '2023-01-09 11:28:05', 'admin', '2023-01-09 15:23:00');
+INSERT INTO `sys_dict_item` VALUES ('449f7f3ff1bf55ddb8159f5d122e29c4', 'sys_log_type', '其他操作', '其他操作', 1, 'default', 0, NULL, 'admin', '2023-01-09 11:27:39', 'admin', '2023-01-09 15:43:04');
+INSERT INTO `sys_dict_item` VALUES ('5bfc739984836091b77b298d9a605181', 'request_type', 'DELETE', 'DELETE', 4, 'red', 0, '删除', 'admin', '2023-01-09 16:13:42', '', NULL);
+INSERT INTO `sys_dict_item` VALUES ('5c24ec491e462910d3123ac7efffdac2', 'sys_log_type', '登录', '登录', 9, 'orange', 0, NULL, 'admin', '2023-01-09 11:28:39', 'admin', '2023-01-09 15:23:43');
+INSERT INTO `sys_dict_item` VALUES ('60f7b458a56b372bcd4c8d797faf926e', 'sys_log_type', '登出', '登出', 10, 'red', 0, NULL, 'admin', '2023-01-09 11:28:50', 'admin', '2023-01-09 15:23:36');
+INSERT INTO `sys_dict_item` VALUES ('6c4f687dc49bf0f4e881d5768fd9ed4d', 'sys_log_type', '修改', '修改', 4, 'purple', 0, NULL, 'admin', '2023-01-09 11:27:58', 'admin', '2023-01-09 16:11:35');
+INSERT INTO `sys_dict_item` VALUES ('6f45f21f466658f51bdbb942f3a679b7', 'sys_log_type', '导出', '导出', 8, '#87d068', 0, NULL, 'admin', '2023-01-09 11:28:30', 'admin', '2023-01-09 15:26:03');
+INSERT INTO `sys_dict_item` VALUES ('731a23dbfd6ec285c9c81ab2a29b43b1', 'sys_log_type', '授权', '授权', 6, 'cyan', 0, NULL, 'admin', '2023-01-09 11:28:16', 'admin', '2023-01-09 16:11:27');
+INSERT INTO `sys_dict_item` VALUES ('75d8887dbeb5a0ca4d20ea445139e97a', 'sys_log_type', '导入', '导入', 7, '#2db7f5', 0, NULL, 'admin', '2023-01-09 11:28:23', 'admin', '2023-01-09 15:25:53');
+INSERT INTO `sys_dict_item` VALUES ('8d0e789e6e63e994b31750ad9cb637b4', 'sys_db_type', 'oracle', 'oracle', 2, NULL, 0, 'oracle数据库', 'admin', '2023-01-04 17:32:59', '', NULL);
+INSERT INTO `sys_dict_item` VALUES ('93a1cdf50fe9f17e399cf663193a7a1f', 'request_type', 'GET', 'GET', 1, 'green', 0, '查询', 'admin', '2023-01-09 16:10:49', 'admin', '2023-01-09 16:12:51');
+INSERT INTO `sys_dict_item` VALUES ('ce8059c1fb3b533339f6bdccc17e0046', 'sys_user_sex', '男', '1', 1, 'green', 0, '男性', 'admin', '2023-01-04 17:31:35', 'admin', '2023-01-09 16:09:49');
+INSERT INTO `sys_dict_item` VALUES ('cf2899ae2d41fd29b5cef8de005098e7', 'request_type', 'PUT', 'PUT', 3, 'purple', 0, '修改', 'admin', '2023-01-09 16:12:11', 'admin', '2023-01-09 16:13:19');
+INSERT INTO `sys_dict_item` VALUES ('ec9cfc538a91de2f61829e61064d636b', 'sys_user_sex', '女', '1', 2, 'red', 0, '女性', 'admin', '2023-01-04 17:31:53', 'admin', '2023-01-09 16:09:52');
+INSERT INTO `sys_dict_item` VALUES ('f88a8a4a391e3ae455ba6d8157f22260', 'request_type', 'POST', 'POST', 2, 'blue', 0, '新增', 'admin', '2023-01-09 16:12:41', '', NULL);
 
 -- ----------------------------
 -- Table structure for sys_log
@@ -78,18 +96,18 @@ CREATE TABLE `sys_log`  (
   `method` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '方法',
   `req_type` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '请求类型',
   `req_uri` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '请求路径',
-  `req_param` varchar(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '请求参数',
+  `req_param` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '请求参数',
   `req_source` int(1) NULL DEFAULT 0 COMMENT '请求来源（0其它 1后台用户 2手机端用户）',
   `oper_type` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '0' COMMENT '操作类型（0其它 1查询 2新增 3修改 4删除 5授权 6导入 7导出...）',
   `oper_ip` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '操作IP',
   `oper_status` int(1) NULL DEFAULT 0 COMMENT '操作状态（0正常 1异常）',
-  `remark` varchar(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '描述信息',
+  `remark` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '返回信息',
   `create_by` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '创建用户',
   `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
   `update_by` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '更新用户',
   `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `create_time_index`(`create_time`) USING BTREE
+  INDEX `create_time_index`(`create_time`) USING BTREE COMMENT '操作时间索引'
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '系统日志' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
