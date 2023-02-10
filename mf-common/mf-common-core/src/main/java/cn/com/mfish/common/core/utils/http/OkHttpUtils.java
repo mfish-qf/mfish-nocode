f8831ddabad04eb2b48f8559484e2687318341f5
qiufeng <qiufeng9862@qq.com>
2023-02-10 22:22:13 +0800

    feat: okhttp通用类完善

--- mf-common/mf-common-core/src/main/java/cn/com/mfish/common/core/utils/http/OkHttpUtils.java
+++ mf-common/mf-common-core/src/main/java/cn/com/mfish/common/core/utils/http/OkHttpUtils.java
@@ -9,9 +9,14 @@ import lombok.extern.slf4j.Slf4j;
 import okhttp3.*;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.net.ssl.*;
 import java.io.File;
 import java.io.IOException;
+import java.security.KeyStore;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.text.MessageFormat;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -194,8 +199,8 @@ public class OkHttpUtils {
         }
         HttpType httpType = HttpType.getHttpType(url);
         if (httpType == HttpType.HTTPS) {
-            clientBuilder.sslSocketFactory(SSLUtils.getSSLSocketFactory(), SSLUtils.getX509TrustManager())
-                    .hostnameVerifier(SSLUtils.getHostnameVerifier());
+            clientBuilder.sslSocketFactory(SSLBuild.getSSLSocketFactory(), SSLBuild.getX509TrustManager())
+                    .hostnameVerifier(SSLBuild.getHostnameVerifier());
         }
         return clientBuilder.build();
     }
@@ -256,4 +261,59 @@ public class OkHttpUtils {
         }
     }
 
+    public static class SSLBuild {
+        //获取SSLSocketFactory
+        public static SSLSocketFactory getSSLSocketFactory() {
+            try {
+                SSLContext sslContext = SSLContext.getInstance("SSL");
+                sslContext.init(null, getTrustManager(), new SecureRandom());
+                return sslContext.getSocketFactory();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        //获取TrustManager
+        private static TrustManager[] getTrustManager() {
+            return new TrustManager[]{
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(X509Certificate[] chain, String authType) {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(X509Certificate[] chain, String authType) {
+                        }
+
+                        @Override
+                        public X509Certificate[] getAcceptedIssuers() {
+                            return new X509Certificate[]{};
+                        }
+                    }
+            };
+        }
+
+        //获取HostnameVerifier
+        public static HostnameVerifier getHostnameVerifier() {
+            return (s, sslSession) -> true;
+        }
+
+        public static X509TrustManager getX509TrustManager() {
+            X509TrustManager trustManager = null;
+            try {
+                TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+                trustManagerFactory.init((KeyStore) null);
+                TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
+                if (trustManagers.length != 1 || !(trustManagers[0] instanceof X509TrustManager)) {
+                    throw new IllegalStateException("Unexpected default trust managers:" + Arrays.toString(trustManagers));
+                }
+                trustManager = (X509TrustManager) trustManagers[0];
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return trustManager;
+        }
+
+    }
 }
