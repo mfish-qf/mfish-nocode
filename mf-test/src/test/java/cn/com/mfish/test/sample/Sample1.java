1421921fefe82469a5b3383d67e1d43feb71b4ac
qiufeng <qiufeng9862@qq.com>
2023-03-05 16:16:11 +0800

    feat: 增加序列ID生成类

---
+++ mf-test/src/test/java/cn/com/mfish/test/sample/Sample1.java
@@ -0,0 +1,32 @@
+package cn.com.mfish.test.sample;
+
+import cn.com.mfish.common.redis.common.IDBuild;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * @description: 测试类
+ * @author: qiufeng
+ * @date: 2023/3/5 14:59
+ */
+@Slf4j
+@SpringBootTest
+@ComponentScan(basePackages = "cn.com.mfish.test")
+public class Sample1 {
+
+    @Test
+    public void testIDBuild() {
+        for (int i = 0; i < 2000; i++) {
+            CompletableFuture.runAsync(() -> System.out.println(IDBuild.getID("File")));
+        }
+        try {
+            Thread.sleep(60000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
