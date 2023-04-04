499aec8fb44f8a0be321dbef42a3dd16d8b86785
qiufeng <qiufeng9862@qq.com>
2023-04-05 00:40:57 +0800

    feat: oracle过滤行标识字段

--- mf-common/mf-common-dblink/src/main/java/cn/com/mfish/common/dblink/common/Constant.java
+++ mf-common/mf-common-dblink/src/main/java/cn/com/mfish/common/dblink/common/Constant.java
@@ -10,4 +10,5 @@ public class Constant {
     public static final String IndexOverException = "错误:超出索引界限";
     public static final String ErrorRowException = "错误:行不匹配";
     public static final String EmptyColNameException = "错误:列名不允许为空";
+    public static final String ORACLE_ROW = "ROWNUM_";
 }
--- mf-common/mf-common-dblink/src/main/java/cn/com/mfish/common/dblink/page/dialect/OracleDialect.java
+++ mf-common/mf-common-dblink/src/main/java/cn/com/mfish/common/dblink/page/dialect/OracleDialect.java
@@ -4,6 +4,8 @@ import cn.com.mfish.common.dblink.page.BoundSql;
 import cn.com.mfish.common.dblink.query.BaseQuery;
 import com.github.pagehelper.Page;
 
+import static cn.com.mfish.common.dblink.common.Constant.ORACLE_ROW;
+
 /**
  * @description: oracle相关方言
  * @author: mfish
@@ -18,10 +20,10 @@ public class OracleDialect extends AbstractDialect {
     protected BoundSql getPageSql(BoundSql boundSql, Page page) {
         StringBuilder sqlBuilder = new StringBuilder(boundSql.getSql().length() + 120);
         sqlBuilder.append("SELECT * FROM ( ");
-        sqlBuilder.append(" SELECT TMP_PAGE.*, ROWNUM ROWNUM_ FROM (");
+        sqlBuilder.append(" SELECT TMP_PAGE.*, ROWNUM " + ORACLE_ROW + " FROM (");
         sqlBuilder.append(boundSql.getSql());
         sqlBuilder.append(") TMP_PAGE)");
-        sqlBuilder.append(" WHERE ROWNUM_ <= ? AND ROWNUM_ > ?");
+        sqlBuilder.append(" WHERE " + ORACLE_ROW + " <= ? AND " + ORACLE_ROW + " > ?");
         boundSql.getParams().add(page.getEndRow());
         boundSql.getParams().add(page.getStartRow());
         return boundSql.setSql(sqlBuilder.toString());
