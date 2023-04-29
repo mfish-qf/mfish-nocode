c520812d7a1f52f0dc86c26d8516260d300959b9
qiufeng <qiufeng9862@qq.com>
2023-04-29 23:27:27 +0800

    feat: 增加代码生成查询条件

--- mf-business/mf-sys/src/main/java/cn/com/mfish/sys/req/ReqCodeBuild.java
+++ mf-business/mf-sys/src/main/java/cn/com/mfish/sys/req/ReqCodeBuild.java
@@ -1,6 +1,7 @@
 package cn.com.mfish.sys.req;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -14,5 +15,9 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 @ApiModel("代码构建请求参数")
 public class ReqCodeBuild {
-
+    private String tableName;
+    @ApiModelProperty(value = "接口路径前缀 例如:/oauth2/user接口前缀为oauth2(不传会使用packageName，最底层包名 例如:cn.com.mfish.sys包会使用sys)")
+    private String apiPrefix;
+    @ApiModelProperty(value = "实体类名(不传会使用表名驼峰化)")
+    private String entityName;
 }
