d3923cf16bc1c0b30d48117d4493bb23e356c92a
qiufeng <qiufeng9862@qq.com>
2023-07-13 14:32:19 +0800

    feat: 升级fastjson2

--- mf-common/mf-common-dblink/src/test/java/cn/com/mfish/common/dblink/TestConnect.java
+++ mf-common/mf-common-dblink/src/test/java/cn/com/mfish/common/dblink/TestConnect.java
@@ -1,17 +1,17 @@
 package cn.com.mfish.common.dblink;
 
 import cn.com.mfish.common.core.enums.DataType;
-import cn.com.mfish.common.dataset.enums.TargetType;
 import cn.com.mfish.common.dataset.datatable.MetaDataHeader;
 import cn.com.mfish.common.dataset.datatable.MetaDataHeaders;
 import cn.com.mfish.common.dataset.datatable.MetaDataRow;
 import cn.com.mfish.common.dataset.datatable.MetaDataTable;
+import cn.com.mfish.common.dataset.enums.TargetType;
 import cn.com.mfish.common.dblink.dbpool.PoolWrapper;
 import cn.com.mfish.common.dblink.entity.DataSourceOptions;
 import cn.com.mfish.common.dblink.enums.DBType;
 import cn.com.mfish.common.dblink.enums.PoolType;
 import cn.com.mfish.common.dblink.manger.PoolManager;
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import org.junit.Test;
 
 import java.sql.Connection;
--- mf-common/mf-common-dblink/src/test/java/cn/com/mfish/common/dblink/TestQuery.java
+++ mf-common/mf-common-dblink/src/test/java/cn/com/mfish/common/dblink/TestQuery.java
@@ -8,7 +8,7 @@ import cn.com.mfish.common.dblink.enums.DBType;
 import cn.com.mfish.common.dblink.enums.PoolType;
 import cn.com.mfish.common.dblink.page.MfPageHelper;
 import cn.com.mfish.common.dblink.query.QueryHandler;
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import org.junit.Test;
 
 import java.util.ArrayList;
