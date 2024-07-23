9c46c6b5909595348046d355186213d64388c204
qiufeng <qiufeng9862@qq.com>
2024-07-23 11:08:38 +0800

    feat: 解决monitor过时方法

--- mf-monitor/src/main/java/cn/com/mfish/monitor/config/MonitorSecurityConfig.java
+++ mf-monitor/src/main/java/cn/com/mfish/monitor/config/MonitorSecurityConfig.java
@@ -3,7 +3,9 @@ package cn.com.mfish.monitor.config;
 import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -27,26 +29,29 @@ public class MonitorSecurityConfig {
         SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
         successHandler.setTargetUrlParameter("redirectTo");
         successHandler.setDefaultTargetUrl(adminContextPath + "/");
-        http.authorizeHttpRequests()
-                //无需登录即可访问
-                .requestMatchers(adminContextPath + "/assets/**",
-                        adminContextPath + "/login",
-                        adminContextPath + "/actuator/health").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                //登录和登出路径
-                .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
-                .logout().logoutUrl(adminContextPath + "/logout").and()
+        http.authorizeHttpRequests((auth) -> {
+                    auth.requestMatchers(adminContextPath + "/assets/**",
+                                    adminContextPath + "/login",
+                                    adminContextPath + "/actuator/health").permitAll()
+                            .anyRequest().authenticated();
+                }).formLogin((login) -> {
+                    login.loginPage(adminContextPath + "/login").successHandler(successHandler);
+                }).logout((logout) -> {
+                    logout.logoutUrl(adminContextPath + "/logout");
+                })
                 //开启http basic支持，admin-client注册时需要使用
-                .httpBasic().and()
+                .httpBasic(Customizer.withDefaults())
                 // 允许嵌入到iframe中
-                .headers().frameOptions().disable().and()
-                .csrf()
+                .headers((header) -> {
+                    header.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable);
+                })
                 //开启基于cookie的csrf保护
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).ignoringRequestMatchers(
-                        adminContextPath + "/instances",
-                        adminContextPath + "/actuator/**"
-                );
+                .csrf((csrf) -> {
+                    csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).ignoringRequestMatchers(
+                            adminContextPath + "/instances",
+                            adminContextPath + "/actuator/**"
+                    );
+                });
         return http.build();
     }
 }
